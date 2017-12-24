08f0a437e69da4a23628132796792b136d65e8cf
antonMeln <32076906+antonMeln@users.noreply.github.com>
2017-12-24 21:51:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,21 +8,24 @@ bool read( int &n, int *mas)
   getline(cin,str);
   istringstream stream(str);
   bool f =true;
+  
   for (int i =0; i < n; i++){
-    if (!(stream >> mas[i]) && (mas[i]>255)) {f =false; break;}
+    if (!(stream >> mas[i]) && mas[i]>255) {
+      f =false; 
+      break;
+      
     }
+  }
   return f;
- } 
-void CountingSort(int *a, int *c, int n, int k)
-{
-    for (int i = 0; i < k; i++) {
-        c[i] = 0;
-    }
+} 
+void CountingSort(int *a,  int n)
+{   int c[256] = {0};
+    
     for (int i = 0; i < n; i++) {
         c[a[i]] = c[a[i]] + 1;
     }
     int i = 0;
-    for (int j = 0; j < k; j++) {
+    for (int j = 0; j < 256; j++) {
         while (c[j] != 0) {
             a[i] = j;
             c[j]--;
@@ -31,9 +34,9 @@ void CountingSort(int *a, int *c, int n, int k)
     }
 }
 int main()
-{    int k=255;
+{  
     int *a= new int;
-    int *c = new int [k];
+    
     int n,i;
     string str;
     getline(cin,str);
@@ -41,15 +44,13 @@ int main()
     
     if (stream >> n){
       if (read(n,a)){
-        CountingSort( a, c, n, k);
-        
-      
-      for (int i = 0; i < n; i++)
-       {
-       cout << a[i] << " ";
-       }
-    }  
-else cout <<"An error has occurred while reading numbers";
-}
+        CountingSort( a, n);
+ 
+        for (int i = 0; i < n; i++){
+           cout << a[i] << " ";
+        }
+      }  
+      else cout <<"An error has occurred while reading numbers";
+    } 
    return 0;
 }
